31b98fb41f6706fc374ba3fed67a43eac001c875
pauljsyi <pauljsyi@gmail.com>
2022-02-15 22:35:14 -0800

    first commit

---
+++ src/context/shopContext.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Client from "shopify-buy";
+
+const client = Client.buildClient({
+  domain: "your-shop-name.myshopify.com",
+  storefrontAccessToken: "your-storefront-access-token",
+});
+
+export class ShopProvider extends Component {
+  render() {
+    return <div>shopContext</div>;
+  }
+}
+
+export default ShopProvider;
