75412943ec4e9d66aa014190f16ac3ec3623908e
pauljsyi <pauljsyi@gmail.com>
2022-02-18 18:00:18 -0800

    styled homepage using Grid

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { ShopContext } from "../context/shopContext";
+import { Box, Grid, Text, Image } from "@chakra-ui/react";
 
 const Home = () => {
   const { fetchAllProducts, products } = useContext(ShopContext);
@@ -9,15 +10,21 @@ const Home = () => {
     fetchAllProducts();
   }, [fetchAllProducts]);
 
+  console.log(products);
+
   if (!products) return <div>Loading...</div>;
   return (
-    <div>
-      {products.map((product) => (
-        <Link to={`/products/${product.handle}`} key={product.title}>
-          {product.title}
-        </Link>
-      ))}
-    </div>
+    <Box>
+      <Grid templateColumns="repeat(3, 1fr)">
+        {products.map((product) => (
+          <Link to={`/products/${product.handle}`} key={product.id}>
+            <Image src={product.images[0].src}></Image>
+            <Text>{product.title}</Text>
+            <Text>$ {product.variants[0].price}</Text>
+          </Link>
+        ))}
+      </Grid>
+    </Box>
   );
 };
 
