5b49584f3f2e5f641229524c440f8ae16e1781af
pauljsyi <pauljsyi@gmail.com>
2022-02-19 14:58:46 -0800

    updated readme

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,9 +18,11 @@ const Home = () => {
       <Grid templateColumns="repeat(3, 1fr)">
         {products.map((product) => (
           <Link to={`/products/${product.handle}`} key={product.id}>
-            <Image src={product.images[0].src}></Image>
-            <Text>{product.title}</Text>
-            <Text>$ {product.variants[0].price}</Text>
+            <Box _hover={{ opacity: "80%" }} textAlign="center">
+              <Image src={product.images[0].src}></Image>
+              <Text>{product.title}</Text>
+              <Text>$ {product.variants[0].price}</Text>
+            </Box>
           </Link>
         ))}
       </Grid>
