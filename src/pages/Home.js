004c78780d6858e65ca035573317528233a48c9d
pauljsyi <pauljsyi@gmail.com>
2022-02-25 12:53:47 -0800

    added richtext

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import { ShopContext } from "../context/shopContext";
 import { Box, Grid, Text, Image } from "@chakra-ui/react";
 import Hero from "../components/Hero";
 import ImageWithText from "../components/ImageWithText";
-
+import RichText from "../components/RichText";
 const Home = () => {
   const { fetchAllProducts, products } = useContext(ShopContext);
 
@@ -16,17 +16,33 @@ const Home = () => {
   return (
     <Box>
       <Hero />
+      <RichText
+        heading="Lorem Ipsum"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      />
       <Grid templateColumns="repeat(3, 1fr)">
         {products.map((product) => (
           <Link to={`/products/${product.handle}`} key={product.id}>
-            <Box _hover={{ opacity: "80%" }} textAlign="center">
+            <Box
+              _hover={{ opacity: "80%" }}
+              textAlign="center"
+              position="relative"
+            >
               <Image src={product.images[0].src}></Image>
-              <Text>{product.title}</Text>
-              <Text>$ {product.variants[0].price}</Text>
+              <Text position="absolute" bottom="15%" w="100%" fontWeight="bold">
+                {product.title}
+              </Text>
+              <Text position="absolute" bottom="5%" w="100%" color="gray">
+                $ {product.variants[0].price}
+              </Text>
             </Box>
           </Link>
         ))}
       </Grid>
+      <RichText
+        heading="Treat Yourself"
+        // text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      />
       <ImageWithText
         reverse
         image="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/premium-bath-bombs.jpg?v=1610066758"
