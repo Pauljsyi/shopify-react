db1479d259256d60fb2055ffa9e8b07189241c14
pauljsyi <pauljsyi@gmail.com>
2022-02-25 12:43:24 -0800

    created subhero section with styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { ShopContext } from "../context/shopContext";
 import { Box, Grid, Text, Image } from "@chakra-ui/react";
 import Hero from "../components/Hero";
+import ImageWithText from "../components/ImageWithText";
 
 const Home = () => {
   const { fetchAllProducts, products } = useContext(ShopContext);
@@ -26,6 +27,26 @@ const Home = () => {
           </Link>
         ))}
       </Grid>
+      <ImageWithText
+        reverse
+        image="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/premium-bath-bombs.jpg?v=1610066758"
+        heading="Lorem Ipsum"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        "
+      />
+      <ImageWithText
+        image="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/bath-bomb-and-candle.jpg?v=1610066758"
+        heading="Lorem Ipsum"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        "
+      />
+      <ImageWithText
+        reverse
+        image="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/premium-bath-bombs.jpg?v=1610066758"
+        heading="Lorem Ipsum"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        "
+      />
     </Box>
   );
 };
